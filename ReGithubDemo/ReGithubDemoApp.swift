fa16ebd1766a84d1aca91ace292745d40d1efef7
joannarodriguez134 <140280268+joannarodriguez134@users.noreply.github.com>
2023-10-23 01:30:04 -0500

    Initial Commit

---
+++ ReGithubDemo/ReGithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReGithubDemoApp.swift
+//  ReGithubDemo
+//
+//  Created by student on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct ReGithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
